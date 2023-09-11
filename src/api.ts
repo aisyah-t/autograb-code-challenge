b18b189b6309eb39a82e91fabdd9ef773dfc434b
Aisyah Tajudin <aisyah@Aisyahs-MacBook-Pro.local>
2023-09-11 12:57:38 +1000

    Add server API endpoints and client-side post function

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+export async function postData(url: string, data: unknown = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return response.json();
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,24 @@
 import express, { Request, Response } from "express";
+import { MODELS } from "../mockData.ts";
 
 export const app = express();
 
 app.use(express.json());
 
 app.get("/cars", (_req: Request, res: Response) =>
-  res.json({ greetings: "Hello World!" })
+  res.status(200).json(MODELS)
 );
+
+app.post("/upload", (req: Request, res: Response) => {
+  const { make, model, badge, logbook } = req.body;
+
+  if (!make || !model || !badge || !logbook) {
+    res.status(400).json({ error: "Required field missing" });
+  }
+
+  try {
+    res.status(200).send({ make, model, badge, logbook });
+  } catch (error) {
+    console.error(error);
+  }
+});
