3f9e195a7dbf7b9d7bfe26d6bed4e5337ba9db17
Aisyah Tajudin <aisyah@Aisyahs-MacBook-Pro.local>
2023-09-11 13:04:56 +1000

    Build form, end-to-end post functionality and remove unnecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,118 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import "./App.css";
+import {
+  Select,
+  FileUpload,
+  Button,
+  FileTypes,
+  ButtonTypes,
+} from "./components/index.ts";
+import { MODELS } from "./mockData.ts";
+import { postData } from "./api.ts";
 
+function readFileContents(
+  file: File,
+  stateSetter: Dispatch<SetStateAction<string>>
+) {
+  const reader = new FileReader();
 
-async function getGreeting () {
-  const res = await fetch("/api");
-  return await res.json();
+  reader.onload = () => {
+    const content = reader.result as string;
+    stateSetter(content);
+  };
+
+  reader.readAsText(file);
+}
+
+interface Car {
+  make: string;
+  model: string;
+  badge: string;
 }
 
 function App() {
+  const [car, setCar] = useState<Car>({ make: "", model: "", badge: "" });
+  const [fileContent, setFileContent] = useState<string>("");
 
-  const [greeting, setGreeting] = useState("")
+  const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
 
-  useEffect(() => {
-    getGreeting().then((res) => setGreeting(res.greeting));
-  }, []);
+    if (file) {
+      readFileContents(file, setFileContent);
+    }
+  };
+
+  const makeOptions = Object.keys(MODELS).map((make) => {
+    return { name: make, value: make };
+  });
+
+  const modelOptions = car.make
+    ? Object.keys(MODELS[car.make]).map((model) => {
+        return { name: model, value: model };
+      })
+    : [];
+
+  const badgeOptions = car.model
+    ? MODELS[car.make][car.model].map((badge) => {
+        return { name: badge, value: badge };
+      })
+    : [];
+
+  const [uploaded, setUploaded] = useState();
+  console.log("POST request response", uploaded);
+
+  const handleSubmit = async () => {
+    const response = await postData("/upload", {
+      ...car,
+      logbook: fileContent,
+    });
+
+    setUploaded(response);
+  };
 
   return (
-      <div>
-      <h1>Vite + React</h1>
-      <p>Server response: {greeting}</p>
+    <>
+      <h1>Vehicle Selection Form</h1>
+      <div className="flex flex-column container">
+        <Select
+          label="Make"
+          options={makeOptions}
+          value={car.make}
+          onChange={(value) => setCar({ make: value, model: "", badge: "" })}
+        />
+        {car.make && (
+          <Select
+            label="Model"
+            options={modelOptions}
+            value={car.model}
+            onChange={(value) => setCar({ ...car, model: value, badge: "" })}
+          />
+        )}
+        {car.model && (
+          <Select
+            label="Badge"
+            options={badgeOptions}
+            value={car.badge}
+            onChange={(value) => setCar({ ...car, badge: value })}
+          />
+        )}
+        {car.badge && (
+          <>
+            <FileUpload
+              label="Upload logbook"
+              acceptedType={FileTypes.Txt}
+              onChange={(event) => handleFileUpload(event)}
+            />
+            <Button
+              label="Submit"
+              type={ButtonTypes.Button}
+              onClick={handleSubmit}
+            />
+          </>
+        )}
       </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
